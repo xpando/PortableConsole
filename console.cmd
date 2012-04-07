747153be133bd59778ac48068cacf3835ba3f261
David Findley <xpando@gmail.com>
2012-04-07 12:13:22 -0500

    Fixed parameterization of console.cmd and psh.cmd

--- console.cmd
+++ console.cmd
@@ -6,5 +6,7 @@ SET HOMEPATH=%~p0profile
 SET USERPROFILE=%PORTABLEROOT%profile
 SET CONFIG=%USERPROFILE%\console.config
 SET PATH=%PORTABLEROOT%bin;%PORTABLEROOT%bin\console2;%PORTABLEROOT%bin\sysinternals;%PATH%
+SET TAB=PowerShell
+IF NOT "%~1"=="" SET TAB=%1
 
-start console.exe -c "%CONFIG%" -d "%PORTABLEROOT%"
+start console.exe -t %TAB% -c "%CONFIG%" -d "%PORTABLEROOT%"
--- psh.cmd
+++ psh.cmd
@@ -6,6 +6,6 @@ SET HOMEPATH=%~p0profile
 SET USERPROFILE=%PORTABLEROOT%profile
 SET PSPROFILE=%USERPROFILE%\Profile.ps1
 SET PATH=%PORTABLEROOT%bin;%PORTABLEROOT%bin\console2;%PORTABLEROOT%bin\sysinternals;%PATH%
-IF DEFINED %1 SET PROFILE=%1
+IF NOT "%~1"=="" SET PSPROFILE=%USERPROFILE%\%~1
 
 %SystemRoot%\System32\WindowsPowerShell\v1.0\PowerShell.exe -nologo -noexit -noprofile -executionpolicy bypass -file "%PSPROFILE%"
