cc0cd4eca900543f5ffe4d3cdb0f91688e362243
David Findley <xpando@gmail.com>
2012-06-08 06:14:39 -0500

    Updated msysgit download script to latest version

--- setup/Install-msysgit.cmd
+++ setup/Install-msysgit.cmd
@@ -1,5 +1,5 @@
 @echo off
-..\bin\wget.exe -v -O "msysgit.7z" "http://msysgit.googlecode.com/files/PortableGit-1.7.9-preview20120201.7z"
+..\bin\wget.exe -v -O "msysgit.7z" "http://msysgit.googlecode.com/files/PortableGit-1.7.10-preview20120409.7z"
 if exist ..\bin\msysgit rd /S /Q ..\bin\msysgit
 ..\bin\7za.exe x -o"..\bin\msysgit" msysgit.7z
 del msysgit.7z
